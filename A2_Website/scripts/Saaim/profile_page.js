1d84b513d986c0c21f8f6c558acab9228ebfcb3e
Saaim0013 <saai0013@flinders.edu.au>
2026-05-12 08:08:44 +0930

    added cs and js

---
+++ A2_Website/scripts/Saaim/profile_page.js
@@ -0,0 +1,20 @@
+
+
+const applyButton = document.querySelector(".profile-button");
+
+applyButton.addEventListener("click", function(event) {
+    alert("Redirecting to the adoption application form.");
+});
+
+const profileSections = document.querySelectorAll(".profile-info");
+
+profileSections.forEach(section => {
+    section.addEventListener("mouseover", function() {
+        section.style.transform = "scale(1.02)";
+        section.style.transition = "0.3s ease";
+    });
+
+    section.addEventListener("mouseout", function() {
+        section.style.transform = "scale(1)";
+    });
+});
