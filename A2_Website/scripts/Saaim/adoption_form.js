d597a335377b2524959d389a0796897067203850
Saaim0013 <saai0013@flinders.edu.au>
2026-05-11 21:12:57 +0930

    changed html a bit

---
+++ A2_Website/scripts/Saaim/adoption_form.js
@@ -0,0 +1,34 @@
+const form = document.querySelector(".adoption-form");
+
+form.addEventListener("submit", function(e) {
+
+    let name = document.getElementById("fullname").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let phone = document.getElementById("phone").value.trim();
+    let experience = document.getElementById("experience").value.trim();
+
+    if (name.length < 2) {
+        alert("Please enter a valid name");
+        e.preventDefault();
+        return;
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Enter a valid email");
+        e.preventDefault();
+        return;
+    }
+
+    if (phone.length < 8) {
+        alert("Enter a valid phone number");
+        e.preventDefault();
+        return;
+    }
+
+    if (experience.length < 10) {
+        alert("Please write at least 10 characters about your experience");
+        e.preventDefault();
+        return;
+    }
+
+});
