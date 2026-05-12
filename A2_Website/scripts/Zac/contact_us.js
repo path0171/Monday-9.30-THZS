912ad6f5568e02cfac769cf815e4a420bd96c558
path0171 <path0171@flinders.edu.au>
2026-05-12 13:30:45 +0930

    Coded Contact Page
    - Coded:
    HTML, CSS and Java for contact page.

---
+++ A2_Website/scripts/Zac/contact_us.js
@@ -0,0 +1,16 @@
+var form = document.getElementById("contact-form");
+var confirmation = document.getElementById("confirmation");
+var resetBtn = document.getElementById("reset-btn");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  form.style.display = "none";
+  confirmation.style.display = "block";
+});
+
+resetBtn.addEventListener("click", function () {
+  form.reset();
+  confirmation.style.display = "none";
+  form.style.display = "block";
+});
